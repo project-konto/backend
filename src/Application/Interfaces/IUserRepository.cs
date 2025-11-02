a187324bf3fed29edf0e82d04923f1cb8f94ff21
Quatov <nooasarano21@gmail.com>
2025-11-02 18:29:55 +0500

    Changed the logic of registration & login a little bit

--- src/Application/Interfaces/IUserRepository.cs
+++ src/Application/Interfaces/IUserRepository.cs
@@ -1,7 +1,9 @@
-﻿namespace KontoApi.Application.Interfaces;
+﻿using KontoApi.Domain;
+
+namespace KontoApi.Application.Interfaces;
 
 public interface IUserRepository
 {
     Task AddAsync(User user);
-    Task<User?> FindByEmailAsync(string email); // checks if email exists
+    Task<User?> FindByEmailAsync(string email); 
 }
--- src/Application/Users/Registration/Registration.cs
+++ src/Application/Users/Registration/Registration.cs
@@ -1,23 +1,24 @@
 ﻿using KontoApi.Application.Interfaces;
+using KontoApi.Domain;
 
 namespace KontoApi.Application.Users;
 
 public class RegisterUserCommand
 {
-    // Gets data (email, password) from UI and sends to handler
-    public string Email { get; }
-    public string Password { get; }
+    public string Name { get; init; }
+    public string Email { get; init; }
+    public string Password { get; init; }
+}
 
-    public RegisterUserCommand(string email, string password)
-    {
-        Email = email;
-        Password = password;
-    }
+public class RegisterUserResult
+{
+    public Guid UserId { get; init; }
+    public string Name { get; init; }
+    public string Email { get; init; }
 }
 
 public class RegisterUserHandler
 {
-    // Gets data from RegisterUserCommand and processes them
     private readonly IUserRepository usersRepository;
     private readonly IPasswordHasher passwordHasher;
 
@@ -29,33 +30,20 @@ public class RegisterUserHandler
 
     public async Task<RegisterUserResult> Handle(RegisterUserCommand command)
     {
-        var email = command.Email;
-        var password = command.Password;
-        
-        var hashed = passwordHasher.Hash(email, password);
-        var user = new User(email, hashed);
+        var existingUser = await usersRepository.FindByEmailAsync(command.Email);
+        if (existingUser != null)
+            throw new InvalidOperationException($"User with email {command.Email} already exists");
+
+        var hashed = passwordHasher.Hash(command.Password);
+        var user = new User(Guid.NewGuid(), command.Name, command.Email, hashed);
         
         await usersRepository.AddAsync(user);
-        return new RegisterUserResult(user.Id);
+        return new RegisterUserResult
+        {
+            UserId = user.Id,
+            Name = user.Name,
+            Email = user.Email
+        };
     }
 }
 
-public class RegisterUserResult
-{
-    // Outputs the result of processing data
-    public bool isSuccess { get; }
-    public Guid? UserId { get; }
-    public string? ErrorMessage { get; }
-
-    public RegisterUserResult(Guid? userId)
-    {
-        isSuccess = true;
-        UserId = userId;
-    }
-
-    public RegisterUserResult(string errorMessage)
-    {
-        isSuccess = false;
-        ErrorMessage = errorMessage;
-    }
-}
